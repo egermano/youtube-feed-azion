28d2b71ae33fc447b073ffe38e8b2c6ef8c15768
Bruno Germano <bruno.germano@gmail.com>
2025-10-01 16:56:09 -0300

    chore: azion deployment

--- azion.config.mjs
+++ azion.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'azion';
+import { defineConfig } from "azion";
 
 export default defineConfig({
   build: {
-    entry: 'src/index.ts',
+    entry: "src/index.ts",
     worker: true,
-    preset: {
-      name: 'typescript',
-    },
+    preset: "javascript",
   },
 });
